389decac8712ec7d2c3fea18bc07e2f38348d78d
barbara <marcelineabadir527@gmail.com>
2023-07-31 13:52:45 +0200

    lottie animation

--- app/src/main/java/com/example/travelapp/WorkInProgressActivity.java
+++ app/src/main/java/com/example/travelapp/WorkInProgressActivity.java
@@ -2,13 +2,30 @@ package com.example.travelapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import com.airbnb.lottie.LottieAnimationView;
 
 public class WorkInProgressActivity extends AppCompatActivity {
 
+    LottieAnimationView animation;
+    ImageView imgMainMenu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_work_in_progress);
+        animation=findViewById(R.id.animation);
+        imgMainMenu=findViewById(R.id.imgMainMenu);
+        imgMainMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(WorkInProgressActivity.this,MainActivity.class));
+            }
+        });
+        //animation.playAnimation();
     }
 }
